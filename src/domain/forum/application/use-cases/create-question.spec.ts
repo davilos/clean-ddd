3a060c1e5cfd7799b82858acd012dc22f8905eda
Dávilos <daviloscostagg@hotmail.com>
2024-06-08 14:42:08 -0300

    test: Teste unitário para o use case de criar Question

---
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,20 @@
+import { QuestionsRepository } from '../repositories/questions-repository'
+import { Question } from '../../enterprise/entities/question'
+import { CreateQuestionUseCase } from './create-question'
+
+const fakeQuestionsRepository: QuestionsRepository = {
+  create: async (question: Question) => {},
+}
+
+test('create a question', async () => {
+  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository)
+
+  const { question } = await createQuestion.execute({
+    authorId: '1',
+    content: 'Pergunta',
+    title: 'Nova pergunta',
+  })
+
+  expect(question.title).toEqual('Nova resposta')
+  expect(question.id).toBeTruthy()
+})
